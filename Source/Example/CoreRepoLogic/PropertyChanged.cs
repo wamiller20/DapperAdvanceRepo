67e388387df855d1b21e5aefde14a198bebedf01
William Miller <wamiller20@hotmail.com>
2023-02-20 19:57:45 -0600

    Example

---
+++ Source/Example/CoreRepoLogic/PropertyChanged.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuplesInterface.CoreRepoLogic
+{
+    public abstract class PropertyChanged
+    {
+        public PropertyChanged()
+        {
+            ChangedProperties = new List<string>();
+        }
+        public List<string> ChangedProperties { get; internal protected set; }
+        protected internal virtual void PropertyHasChanged([CallerMemberName] string propertyName = "")
+        {
+            if (ChangedProperties.Contains(propertyName) == false) { ChangedProperties.Add(propertyName); }
+        }
+
+        public virtual void RemovePropertyFromBeingUpdated(string propertyName)
+        {
+            if (ChangedProperties.Contains(propertyName)) { ChangedProperties.Remove(propertyName); }
+        }
+
+        public virtual void ResetPropertyChanges()
+        {
+            ChangedProperties.Clear();
+        }
+    }
+}
